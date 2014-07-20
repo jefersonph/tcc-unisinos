38733dceee619307380c60ad3c88caed89539556
Jeferson Pizzolato Homem <jefersonph@gmail.com>
2014-07-20 20:29:11 -0300

    adicionando info pessoal e doencas

--- app.js
+++ app.js
@@ -43,8 +43,65 @@ app.get('/mapa', function(req, res){
     res.render('mapa');
 });
 
+app.get('/informacoes', function(req, res){
+    res.render('informacoes');
+});
+
+app.get('/doencas', function(req, res){
+    res.render('doencas');
+});
+
+app.get('/info_pessoais', function(req, res){
+    res.render('info_pessoais/info');
+});
+
+app.get('/info_doencas', function(req, res){
+    res.render('info_pessoais/doencas');
+});
+
+app.get('/adicionar_doenca', function(req, res){
+    res.render('info_pessoais/adicionar_doenca');
+});
+
+app.get('/adicionar_remedio', function(req, res){
+    res.render('info_pessoais/adicionar_remedio');
+});
+
+app.get('/info_remedios', function(req, res){
+    res.render('info_pessoais/remedios');
+});
+
+app.get('/adicionar_alergia', function(req, res){
+    res.render('info_pessoais/adicionar_alergia');
+});
+
+app.get('/info_alergias', function(req, res){
+    res.render('info_pessoais/alergias');
+});
+
 app.get('/estado', function(req, res){
     res.render('estados/estado');
+});
+
+
+app.get('/aids', function(req, res){
+    res.render('doencas/aids');
+});
+
+app.get('/dengue', function(req, res){
+    res.render('doencas/dengue');
+});
+
+app.get('/tuberculose', function(req, res){
+    res.render('doencas/tuberculose');
+});
+
+app.get('/leptospirose', function(req, res){
+    res.render('doencas/leptospirose');
+});
+
+app.get('/aids_detalhe', function(req, res){
+    res.render('doencas/aids_detalhe');
 });
 
 var port = Number(process.env.PORT || 4000);
